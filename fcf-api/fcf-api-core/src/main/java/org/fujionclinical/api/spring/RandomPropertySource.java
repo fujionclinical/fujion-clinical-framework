d9c1eb8e91c3ebe750190c3d60c5553a7dc509b5
Douglas Martin <mdgeek1@gmail.com>
2023-10-20 08:23:29 -0400

    Replace deprecated RandomUtils with commons-rng library.

--- fcf-api/fcf-api-core/src/main/java/org/fujionclinical/api/spring/RandomPropertySource.java
+++ fcf-api/fcf-api-core/src/main/java/org/fujionclinical/api/spring/RandomPropertySource.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -26,7 +26,8 @@
 package org.fujionclinical.api.spring;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.simple.RandomSource;
 import org.fujion.common.Assert;
 import org.springframework.core.env.PropertySource;
 
@@ -37,6 +38,8 @@ import java.util.UUID;
  */
 public class RandomPropertySource extends PropertySource<Object> {
 
+    public static final UniformRandomProvider RANDOM_PROVIDER = RandomSource.XO_RO_SHI_RO_128_PP.create();
+
     private enum RandomType {INT, INTEGER, LONG, VALUE, UUID}
 
     private static final String RANDOM_PREFIX = "random.";
@@ -44,7 +47,7 @@ public class RandomPropertySource extends PropertySource<Object> {
     public RandomPropertySource() {
         super("Random");
     }
-    
+
     /**
      * Label names must be prefixed with "random." to be recognized as such.
      */
@@ -58,9 +61,9 @@ public class RandomPropertySource extends PropertySource<Object> {
             switch (RandomType.valueOf(name.substring(RANDOM_PREFIX.length()).toUpperCase())) {
                 case INT:
                 case INTEGER:
-                    return Integer.toString(RandomUtils.nextInt());
+                    return Integer.toString(RANDOM_PROVIDER.nextInt());
                 case LONG:
-                    return Long.toString(RandomUtils.nextLong());
+                    return Long.toString(RANDOM_PROVIDER.nextLong());
                 case VALUE:
                     return RandomStringUtils.randomAlphanumeric(32);
                 case UUID:
@@ -72,5 +75,5 @@ public class RandomPropertySource extends PropertySource<Object> {
             return Assert.fail("Unknown random property type '%s'", name);
         }
     }
-    
+
 }
